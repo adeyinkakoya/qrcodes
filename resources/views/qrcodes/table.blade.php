6619efc537cfb83decf36de20d7d5b915e017da3
adeyinkakoya <adeyinkakoya@gmail.com>
2019-04-03 17:52:00 +0100

    wedapr3

--- resources/views/qrcodes/table.blade.php
+++ resources/views/qrcodes/table.blade.php
@@ -28,7 +28,7 @@
             <td>
                 {!! Form::open(['route' => ['qrcodes.destroy', $qrcode->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
-                    <a href="{!! route('qrcodes.show', [$qrcode->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
+                    <a href="{!! route('qrcodes.show', [$qrcode->id]) !!}" class='btn btnP-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
                     <a href="{!! route('qrcodes.edit', [$qrcode->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
                     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                 </div>
